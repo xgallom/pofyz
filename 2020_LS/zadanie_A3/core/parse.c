0ef61160ed53e70f1dd73ce97a8cb16e64009cd3
Milan Gallo <gallo.milan.jr@gmail.com>
2020-03-25 18:21:09 +0100

    Add parseDoubles function to core
    Signed-off-by: Milan Gallo <gallo.milan.jr@gmail.com>

--- 2020_LS/zadanie_A3/core/parse.c
+++ 2020_LS/zadanie_A3/core/parse.c
@@ -10,11 +10,26 @@
 
 double parseDouble(const char *string)
 {
-	char *end = NULL;
+	double result;
 
-	const double result = strtod(string, &end);
-
-	fail(end == string || errno == ERANGE, "Failed converting \"%s\" to double\n", string);
+	fail(parseDoubles(string, &result, 1) != 1, "Failed converting \"%s\" to double\n", string);
 
 	return result;
+}
+
+size_t parseDoubles(const char *string, double *buffer, size_t count)
+{
+	const size_t total = count;
+	char *end = NULL;
+
+	for(count = 0; count < total; ++count) {
+		*buffer++ = strtod(string, &end);
+
+		if(end == string || errno == ERANGE)
+			break;
+
+		string = end;
+	}
+
+	return count;
 }
--- 2020_LS/zadanie_A3/core/parse.h
+++ 2020_LS/zadanie_A3/core/parse.h
@@ -5,6 +5,9 @@
 #ifndef POFYZ_2020_LS_ZADANIE_A2_CORE_PARSE_H
 #define POFYZ_2020_LS_ZADANIE_A2_CORE_PARSE_H
 
+#include <stddef.h>
+
 double parseDouble(const char *string);
+size_t parseDoubles(const char *string, double *buffer, size_t count);
 
 #endif //POFYZ_2020_LS_ZADANIE_A2_CORE_PARSE_H
