cf2906164649823fb94bd701ce6955bb99f46920
Milan Gallo <gallo.milan.jr@gmail.com>
2020-04-02 23:27:39 +0200

    Add new functionality to main
    Signed-off-by: Milan Gallo <gallo.milan.jr@gmail.com>

--- 2020_LS/zadanie_A3/main.c
+++ 2020_LS/zadanie_A3/main.c
@@ -1,6 +1,13 @@
 #include "core/rootPath.h"
 #include "arguments/arguments.h"
 #include "data/temperature.h"
+#include "core/matrix.h"
+#include "solvers/linearLeastSquares.h"
+#include "equations/polynomial.h"
+
+#include <stdio.h>
+
+#define SEPARATOR for(size_t _n = 0; _n < 80; ++_n) putchar('-');
 
 int main(int argc, char *argv[])
 {
@@ -11,6 +18,29 @@ int main(int argc, char *argv[])
 	initializeTemperature(&arguments);
 
 	dumpTemperature();
+
+	SEPARATOR
+	printf("\nLinearne fitovanie:\n\n");
+	struct Matrix coefficients = solveLinearLeastSquares(temperaturePositions(), temperatureValues(), 5);
+
+	SEPARATOR
+	printf("\nKoeficienty este nie su vyratane, preto som ich naplnil postupne klesajucimi hodnotami:\n\n");
+	double *coefficient = asMDouble(&coefficients);
+	for(size_t i = 0; i < coefficients.length; ++i)
+		*coefficient++ = ((double) coefficients.length - i) / coefficients.length;
+	dumpMatrix(&coefficients);
+
+	SEPARATOR
+	printf("\nOverenie fungovania funkcie computePolynomial s nedoratanymi koeficientami pre teplotu (Tfun):\n\n");
+	for(double x = 0.0; x < 10.0; x += 0.5) {
+		printf("T(%4.1lf) = ", x);
+
+		coefficient = asMDouble(&coefficients);
+		for(size_t n = 0; n < coefficients.length; ++n)
+			printf("%s %g %3.1f^%d ", n ? "+" : "", *coefficient++, x, n);
+
+		printf("\n        = %f\n\n", computePolynomial(x, &coefficients));
+	}
 
 	cleanupTemperature();
 	cleanupRootPath();
