9f668d8efc94cf54ef1737e047b52b20fd4e2b37
Milan Gallo <gallo.milan.jr@gmail.com>
2020-04-02 23:24:04 +0200

    Add matrix support
    Signed-off-by: Milan Gallo <gallo.milan.jr@gmail.com>

---
+++ 2020_LS/zadanie_A3/core/matrix.c
@@ -0,0 +1,107 @@
+//
+// Created by xgallom on 4/2/20.
+//
+
+#include "matrix.h"
+#include "vector.h"
+
+#include <stdio.h>
+
+struct Matrix matrixDouble(size_t rows, size_t columns)
+{
+	struct Matrix result = {
+			.rows = rows,
+			.columns = columns
+	};
+
+	*((struct Vector *) &result) = vectorDouble(rows * columns);
+
+	return result;
+}
+
+struct Matrix matrixRowVectorDouble(size_t columns) { return matrixDouble(1, columns); }
+struct Matrix matrixColumnVectorDouble(size_t rows) { return matrixDouble(rows, 1); }
+
+struct Matrix matrixCopy(const struct Matrix *from)
+{
+	struct Matrix result = {
+			.rows = from->rows,
+			.columns = from->columns
+	};
+
+	*((struct Vector *) &result) = copy((const struct Vector *) from);
+
+	return result;
+}
+
+size_t matrixResize(struct Matrix *matrix, size_t newRows, size_t newColumns)
+{
+	matrix->rows = newRows == KEEP_SIZE ? matrix->rows : newRows;
+	matrix->columns = newColumns == KEEP_SIZE ? matrix->columns : newColumns;
+
+	return resize((struct Vector *) matrix, matrixSizeOf(matrix));
+}
+void matrixClear(struct Matrix *matrix)
+{
+	matrix->rows = matrix->columns = 0;
+	clear((struct Vector *) matrix);
+}
+void matrixDelete(struct Matrix *matrix)
+{
+	matrix->rows = matrix->columns = 0;
+	delete((struct Vector *) matrix);
+}
+
+int matrixIsNull(const struct Matrix *matrix) { return isNull((const struct Vector *) matrix); }
+size_t matrixSizeOf(const struct Matrix *matrix) { return matrix->rows * matrix->columns; }
+const char *matrixType(const struct Matrix *matrix) { return vectorType((const struct Vector *) matrix); }
+
+void iterateImpl(struct Matrix *matrix, MatrixIterator *iterator, void *data)
+{
+	double *value = asMDouble(matrix);
+
+	for(size_t row = 0; row < matrix->rows; ++row) {
+		for(size_t column = 0; column < matrix->columns; ++column)
+			(*iterator)(matrix, value++, row, column, data);
+	}
+}
+
+void iterateConstImpl(const struct Matrix *matrix, ConstMatrixIterator *iterator, void *data)
+{
+	const double *value = asCMDouble(matrix);
+
+	for(size_t row = 0; row < matrix->rows; ++row) {
+		for(size_t column = 0; column < matrix->columns; ++column)
+			(*iterator)(matrix, value++, row, column, data);
+	}
+}
+
+double *asMDouble(struct Matrix *matrix) { return asDouble((struct Vector *) matrix); }
+const double *asCMDouble(const struct Matrix *matrix) { return asCDouble((const struct Vector *) matrix); }
+double *asRow(struct Matrix *matrix, size_t row) { return asMDouble(matrix) + row * matrix->columns; }
+const double *asCRow(const struct Matrix *matrix, size_t row) { return asCMDouble(matrix) + row * matrix->columns; }
+double getAt(const struct Matrix *matrix, size_t row, size_t column) { return *cAt(matrix, row, column); }
+double *at(struct Matrix *matrix, size_t row, size_t column) { return asRow(matrix, row) + column; }
+const double *cAt(const struct Matrix *matrix, size_t row, size_t column) { return asCRow(matrix, row) + column; }
+
+void dumpMatrixImpl(const struct Matrix *matrix, const char *name)
+{
+	const double *value = asCMDouble(matrix);
+
+	printf(
+			"Matrix \"%s\" of %s[%zu, %zu]:\n"
+			"    ",
+			name,
+			matrixType(matrix),
+			matrix->rows,
+			matrix->columns
+	);
+
+	for(size_t row = 0; row < matrix->rows; ++row) {
+		for(size_t column = 0; column < matrix->columns; ++column)
+			printf("%+6.3e ", *value++);
+
+		printf("\n    ");
+	}
+	printf("\n");
+}
---
+++ 2020_LS/zadanie_A3/core/matrix.h
@@ -0,0 +1,63 @@
+//
+// Created by xgallom on 4/2/20.
+//
+
+#ifndef POFYZ_2020_LS_ZADANIE_A3_CORE_MATRIX_H
+#define POFYZ_2020_LS_ZADANIE_A3_CORE_MATRIX_H
+
+#include "vector.h"
+
+#define KEEP_SIZE -1
+
+struct Matrix {
+	void *data;       // +8B = 8B
+	size_t length;    // +8B = 16B
+	size_t entrySize; // +8B = 24B
+	const char *type; // +8B = 32B
+
+	// 32B/64B = 0.5 L1 cache-line
+
+	size_t rows;      // +8B = 40B
+	size_t columns;   // +8B = 48B
+
+	// 48B/64B = 0.75 L1 cache-line
+};
+
+typedef void (MatrixIterator)(struct Matrix *matrix, double *value, size_t row, size_t column, void *data);
+typedef void (ConstMatrixIterator)(const struct Matrix *matrix,
+								   const double *value,
+								   size_t row,
+								   size_t column, void *data
+);
+
+struct Matrix matrixDouble(size_t rows, size_t columns);
+struct Matrix matrixRowVectorDouble(size_t columns);
+struct Matrix matrixColumnVectorDouble(size_t rows);
+
+struct Matrix matrixCopy(const struct Matrix *from);
+
+size_t matrixResize(struct Matrix *matrix, size_t newRows, size_t newColumns);
+void matrixClear(struct Matrix *matrix);
+void matrixDelete(struct Matrix *matrix);
+
+int matrixIsNull(const struct Matrix *matrix);
+size_t matrixSizeOf(const struct Matrix *matrix);
+const char *matrixType(const struct Matrix *matrix);
+
+void iterateImpl(struct Matrix *matrix, MatrixIterator *iterator, void *data);
+void iterateConstImpl(const struct Matrix *matrix, ConstMatrixIterator *iterator, void *data);
+#define iterate(matrix, iterator, data) iterateImpl(matrix, (MatrixIterator *) iterator, (void *) data)
+#define iterateConst(matrix, iterator, data) iterateConstImpl(matrix, (ConstMatrixIterator *) iterator, (void *) data);
+
+double *asMDouble(struct Matrix *matrix);
+const double *asCMDouble(const struct Matrix *matrix);
+double *asRow(struct Matrix *matrix, size_t row);
+const double *asCRow(const struct Matrix *matrix, size_t row);
+double getAt(const struct Matrix *matrix, size_t row, size_t column);
+double *at(struct Matrix *matrix, size_t row, size_t column);
+const double *cAt(const struct Matrix *matrix, size_t row, size_t column);
+
+void dumpMatrixImpl(const struct Matrix *matrix, const char *name);
+#define dumpMatrix(matrix) dumpMatrixImpl(matrix, #matrix)
+
+#endif //POFYZ_2020_LS_ZADANIE_A3_CORE_MATRIX_H
