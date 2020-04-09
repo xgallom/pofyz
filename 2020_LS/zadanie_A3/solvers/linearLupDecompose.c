b79d83d36f513e1a199f670c37ffaa5be6f027e2
Milan Gallo <gallo.milan.jr@gmail.com>
2020-04-09 22:12:45 +0200

    Add LUP decomposition linear solver
    Signed-off-by: Milan Gallo <gallo.milan.jr@gmail.com>

---
+++ 2020_LS/zadanie_A3/solvers/linearLupDecompose.c
@@ -0,0 +1,52 @@
+//
+// Created by xgallom on 4/9/20.
+//
+
+#include "linearLupDecompose.h"
+#include "../core/matrix.h"
+#include "../core/error.h"
+#include "../core/matrixOperations.h"
+
+#include <math.h>
+
+#define TOLERANCE 1e-20
+
+static size_t findMaximumPivotRow(size_t diagonal, double **index, size_t length)
+{
+	double maximum = 0.0;
+	size_t maximumRow = diagonal;
+
+	for(size_t row = diagonal; row < length; ++row) {
+		const double absolute = fabs(index[row][diagonal]);
+
+		if(absolute > maximum) {
+			maximum = absolute;
+			maximumRow = row;
+		}
+	}
+
+	fail(maximum < TOLERANCE, "LUP: Failed to decompose, matrix is degenerate\n");
+
+	return maximumRow;
+}
+
+void solveLinearLupDecompose(struct Matrix *matrixIndex, struct Vector *pivots)
+{
+	const size_t length = matrixIndex->rows;
+	double **index = asMDoubleIndex(matrixIndex);
+	int *pivot = asInt(pivots);
+
+	for(size_t diagonal = 0; diagonal < length; ++diagonal) {
+		size_t pivotRow = findMaximumPivotRow(diagonal, index, length);
+
+		if(pivotRow != diagonal)
+			matrixPivotSwap(index, pivot, diagonal, pivotRow, length);
+
+		for(size_t row = diagonal + 1; row < length; ++row) {
+			index[row][diagonal] /= index[diagonal][diagonal];
+
+			for(size_t column = diagonal + 1; column < length; ++column)
+				index[row][column] -= index[row][diagonal] * index[diagonal][column];
+		}
+	}
+}
---
+++ 2020_LS/zadanie_A3/solvers/linearLupDecompose.h
@@ -0,0 +1,13 @@
+//
+// Created by xgallom on 4/9/20.
+//
+
+#ifndef POFYZ_2020_LS_ZADANIE_A3_SOLVERS_LINEARLUPDECOMPOSE_H
+#define POFYZ_2020_LS_ZADANIE_A3_SOLVERS_LINEARLUPDECOMPOSE_H
+
+struct Matrix;
+struct Vector;
+
+void solveLinearLupDecompose(struct Matrix *matrixIndex, struct Vector *pivots);
+
+#endif //POFYZ_2020_LS_ZADANIE_A3_SOLVERS_LINEARLUPDECOMPOSE_H
