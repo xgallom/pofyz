3bf80558507307a73a87baeb99b653a7873ab3a4
Milan Gallo <gallo.milan.jr@gmail.com>
2020-04-23 04:58:28 +0200

    Add progress info and maximum simulation limit to generic solver
    Signed-off-by: Milan Gallo <gallo.milan.jr@gmail.com>

--- 2020_LS/zadanie_A3/solvers/solveGeneric.c
+++ 2020_LS/zadanie_A3/solvers/solveGeneric.c
@@ -5,6 +5,9 @@
 #include "solveGeneric.h"
 #include "../core/vector.h"
 #include "../constants.h"
+#include "../core/error.h"
+
+#include <stdio.h>
 
 size_t solveGeneric(SolverIterator solver, struct Vector *x_vec, struct Vector *v_vec)
 {
@@ -22,11 +25,15 @@ size_t solveGeneric(SolverIterator solver, struct Vector *x_vec, struct Vector *
 
 		solver(x + start, v + start, length - start);
 
+		printf("Height: %f m\n", x[length - 1]);
+
 		if(x[length - 1] <= 0.0)
 			break;
 
-		start = resize(x_vec, (length += steps)) - 1;
+		start = resize(x_vec, (length += batchSize)) - 1;
 		resize(v_vec, length);
+
+		fail(length >= simulationLengthLimit, "Simulation length limit exceeded\n");
 	}
 
 	return length;
