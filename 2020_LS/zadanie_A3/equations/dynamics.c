82cedeee8db8fb46ec87100cd91a242eac91cb76
Milan Gallo <gallo.milan.jr@gmail.com>
2020-04-25 16:14:39 +0200

    Pass value of integral into K from acceleration instead of computing it inside
    Signed-off-by: Milan Gallo <gallo.milan.jr@gmail.com>

--- 2020_LS/zadanie_A3/equations/dynamics.c
+++ 2020_LS/zadanie_A3/equations/dynamics.c
@@ -6,29 +6,20 @@
 #include "../data/temperature.h"
 #include "../constants.h"
 
-double reciprocalTemperatureFor(double x)
+static double ro(double x, double integralT)
 {
-	return 1. / temperatureFor(x);
-}
-
-static double ro(double x, double *oldX, double *oldIntegralT)
-{
-	const double delta = x - *oldX;
-	*oldIntegralT += reciprocalTemperatureFor(*oldX + delta / 2.0) * delta;
-	*oldX = x;
-
 	const double
 			T = temperatureFor(x),
 			MR = M / R,
 
 			constant = p_0 * MR / T,
-			exponential = exp(-g * MR * *oldIntegralT);
+			exponential = exp(-g * MR * integralT);
 
 
 	return constant * exponential;
 }
 
-double K(double x, double *oldX, double *oldIntegralT)
+double K(double x, double integralT)
 {
-	return 0.5 * C * S * ro(x, oldX, oldIntegralT) / m;
+	return 0.5 * C * S * ro(x, integralT) / m;
 }
--- 2020_LS/zadanie_A3/equations/dynamics.h
+++ 2020_LS/zadanie_A3/equations/dynamics.h
@@ -5,7 +5,6 @@
 #ifndef POFYZ_2020_LS_ZADANIE_A3_EQUATIONS_DYNAMICS_H
 #define POFYZ_2020_LS_ZADANIE_A3_EQUATIONS_DYNAMICS_H
 
-double reciprocalTemperatureFor(double x);
-double K(double x, double *oldX, double *oldIntegralT);
+double K(double x, double integralT);
 
 #endif //POFYZ_2020_LS_ZADANIE_A3_EQUATIONS_DYNAMICS_H
